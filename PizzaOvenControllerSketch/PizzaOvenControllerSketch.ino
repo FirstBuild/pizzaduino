b1080aca86b5d556fd2be615327b7e3ab9351371
Rob Bultman <rob@firstbuild.com>
2016-12-21 13:13:24 -0500

    Releasing v1.0.10

--- PizzaOvenControllerSketch/PizzaOvenControllerSketch.ino
+++ PizzaOvenControllerSketch/PizzaOvenControllerSketch.ino
@@ -65,7 +65,7 @@ static TcLimitCheck lrTcLimit(1000, 0);
 //------------------------------------------
 #define FIRMWARE_MAJOR_VERSION   1
 #define FIRMWARE_MINOR_VERSION   0
-#define FIRMWARE_BUILD_VERSION   9
+#define FIRMWARE_BUILD_VERSION   10
 
 const char versionString[] = {'V', ' ', '0' + FIRMWARE_MAJOR_VERSION, '.', '0' + FIRMWARE_MINOR_VERSION, ' ', 'b', 'u', 'g', 'f', 'i', 'x', ' ', '0' + FIRMWARE_BUILD_VERSION, 0};
 
